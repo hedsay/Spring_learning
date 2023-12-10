baf99933542f75f6a02d4446362e9fd9d6acb6c4
konvergenz <1141955640@qq.com>
2023-12-10 23:07:05 +0800

    v1.1 add ApplicationContext实现类

---
+++ Spring_learning/src/main/java/com/Nreal/IOC/A2/TestApplicationContext.java
@@ -0,0 +1,92 @@
+package com.Nreal.IOC.A2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class TestApplicationContext {
+    private static final Logger log = LoggerFactory.getLogger(TestApplicationContext.class);
+
+    public static void main(String[] args) {
+        //testAnnotationConfigApplicationContext();
+        testAnnotationConfigServletWebServerApplicationContext();
+    }
+
+    private static void testAnnotationConfigApplicationContext() {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(Config.class);
+        for (String name : context.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+        System.out.println(context.getBean(Bean2.class).getBean1());
+    }
+
+    private static void testAnnotationConfigServletWebServerApplicationContext() {
+        AnnotationConfigServletWebServerApplicationContext context =
+                new AnnotationConfigServletWebServerApplicationContext(WebConfig.class);
+        for (String name : context.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
+    }
+
+    @Configuration
+    static class WebConfig{
+        @Bean
+        public ServletWebServerFactory servletWebServerFactory(){
+            return new TomcatServletWebServerFactory();
+        }
+        @Bean
+        public DispatcherServlet dispatcherServlet() {
+            return new DispatcherServlet();
+        }
+        // 将Servlet注册到tomcat服务器上
+        @Bean
+        public DispatcherServletRegistrationBean registrationBean(DispatcherServlet dispatcherServlet) {
+            return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+        }
+        @Bean("/hello")
+        public Controller controller1() {
+            return (request, response) -> {
+                response.getWriter().print("hello");
+                return null;
+            };
+        }
+    }
+
+    @Configuration
+    static class Config{
+        @Bean
+        public Bean1 bean1(){
+            return new Bean1();
+        }
+        @Bean
+        public Bean2 bean2(Bean1 bean1){
+            Bean2 bean2 = new Bean2();
+            bean2.setBean1(bean1);
+            return bean2;
+        }
+    }
+    static class Bean1{}
+    static class Bean2{
+        private Bean1 bean1;
+        public Bean1 getBean1() {
+            return bean1;
+        }
+        public void setBean1(Bean1 bean1) {
+            this.bean1 = bean1;
+        }
+    }
+}
+
+
+
+
+
