4d9c88c8eed8e3fa8fa879614ccd4b42ad339020
konvergenz <1141955640@qq.com>
2023-12-13 23:24:37 +0800

    v1.1 add Aware与InitializingBean接口

---
+++ Spring_learning/src/main/java/com/Nreal/IOC/A4/MyBean.java
@@ -0,0 +1,43 @@
+package com.Nreal.IOC.A4;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import javax.annotation.PostConstruct;
+
+public class MyBean implements BeanNameAware, ApplicationContextAware, InitializingBean {
+
+    private static final Logger log = LoggerFactory.getLogger(MyBean.class);
+
+    @Override
+    public void setBeanName(String name) {
+        log.debug("当前bean " + this + " 名字叫:" + name);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        log.debug("当前bean " + this + " 初始化");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        log.debug("当前bean " + this + " 容器是:" + applicationContext);
+    }
+
+    // 没有Bean后处理器，下两个方法不生效
+    @Autowired
+    public void NeedBeanPostProcessor(ApplicationContext applicationContext) {
+        log.debug("当前bean " + this + " 使用@Autowired 容器是:" + applicationContext);
+    }
+
+    @PostConstruct
+    public void init() {
+        log.debug("当前bean " + this + " 使用@PostConstruct 初始化");
+    }
+}
---
+++ Spring_learning/src/main/java/com/Nreal/IOC/A4/TestAware.java
@@ -0,0 +1,27 @@
+package com.Nreal.IOC.A4;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
+import org.springframework.context.annotation.ConfigurationClassPostProcessor;
+import org.springframework.context.support.GenericApplicationContext;
+
+public class TestAware {
+    private static final Logger log = LoggerFactory.getLogger(TestAware.class);
+
+    public static void main(String[] args) {
+        GenericApplicationContext context = new GenericApplicationContext();
+//        context.registerBean("myBean", MyBean.class);
+//        context.registerBean("myConfig1", MyConfig1.class);
+        context.registerBean("myConfig2", MyConfig2.class);
+
+        // bean后处理器
+        context.registerBean(AutowiredAnnotationBeanPostProcessor.class);
+        context.registerBean(CommonAnnotationBeanPostProcessor.class);
+        context.registerBean(ConfigurationClassPostProcessor.class);
+
+        context.refresh();
+        context.close();
+    }
+}
